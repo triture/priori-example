d6b6848ffbe0af8420ea89f35873e446276b3e56
Felipe Peternella <felipevex@gmail.com>
2016-05-05 13:10:55 -0300

    Audio fix

--- src/PriDemoApp.hx
+++ src/PriDemoApp.hx
@@ -19,7 +19,7 @@ class PriDemoApp extends PriApp {
         PriFontStyle.DEFAULT_COLOR = 0x19385F;
 
         AssetManager.g().addToQueue(new AssetImage("image_example", "assets/media_image.jpg"));
-        AssetManager.g().addToQueue(new AssetAudio("audio_example", "assets/media_audio.ogg"));
+        AssetManager.g().addToQueue(new AssetAudio("audio_example", "assets/media_audio.mp3", "assets/media_audio.ogg"));
 
         SceneManager.g().preload(ContentScene, PreloadScene, this.onErrorCallBack);
     }
